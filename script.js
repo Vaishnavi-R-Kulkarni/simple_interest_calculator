dee54a3db72a3bc51926049d9082a054c803395b
Vaishnavi-R-Kulkarni <83642770+Vaishnavi-R-Kulkarni@users.noreply.github.com>
2021-10-25 20:26:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function calculate() {
     var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
     var yearInTheFuture = new Date().getFullYear() + years;
+
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
     
     document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
